82b365f2112457a373c7f9691fae29453991d52e
WaleedAlzamil80 <waleedalzamil80@gmail.com>
2023-03-21 23:48:56 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,10 @@
 /**
  * print_sign - my function
  * @n: is the number to be checked
- * Description: checking if the argument n is a greater or less or equal to zero
+ * 
+ * Description: checking if the argument n is a greater 
+ * or less or equal to zero
+ *
  * Return: 1 if the argument n is greater than 1, 0 equal to 0, -1 less than 0
  */
 int print_sign(int n)
