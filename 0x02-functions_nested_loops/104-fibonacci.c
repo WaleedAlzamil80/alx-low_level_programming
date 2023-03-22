c10d7e4d6881d88c5527804795891ea2d9fc3658
WaleedAlzamil80 <waleedalzamil80@gmail.com>
2023-03-22 01:13:34 -0700

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,7 +3,8 @@
  * main - fibonacci <3
  *
  * Purpose - no hardcode
- * Description: prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line
+ * Description: prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line
  *
  * Return:  (Success)
  */
