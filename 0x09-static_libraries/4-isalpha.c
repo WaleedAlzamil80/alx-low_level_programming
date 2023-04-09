5fab0db91fbe2e1072350a86cca509376a1ebbdf
WaleedAlzamil80 <waleedalzamil80@gmail.com>
2023-04-09 08:29:56 -0700

    all for one and one for all

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isalpha - my function
+ * @c: is the character to be checked
+ * Description: checking if theargument c is a letter
+ * Return: 1 if the argument c is alpha, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'));
+}
