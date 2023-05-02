46df57cba4a7deb0945cc3d32d51fa25395d5004
WaleedAlzamil80 <waleedalzamil80@gmail.com>
2023-05-02 13:03:10 -0700

    print lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t list
- * @h: A list to print all of its entries
+ * @h: linked list of type listint_t to print
  *
  * Return: the number of nodes
  */
