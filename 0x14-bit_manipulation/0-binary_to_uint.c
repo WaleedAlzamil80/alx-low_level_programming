267d614f89071636548e072d573c14623d102742
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2023-07-07 12:02:04 +0300

    binary to integre

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0, n = 0, len = 0;
 	unsigned int k = 1;
 
-	while(b[len] != '\0')
+ 	while (b[len] != '\0')
 	{
 		len++;
 	}
-	for(i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
-		if(b[i] != '0' && b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		if(b[i] == '1')
+		if (b[i] == '1')
 			n += k;
 		k *= 2;
 	}
