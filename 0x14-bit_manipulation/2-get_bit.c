4439fef0699b11b1a0260c646eaf3743fb1fffd0
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2023-07-08 00:49:00 +0300

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * get_bit -  value of a bit at a given index.
- * @index: (unsigned int) is the index, starting from 0 
+ * @index: (unsigned int) is the index, starting from 0
  * of the bit you want to get
  *
  * @n: (unsigned long int) the number
@@ -10,12 +10,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int v, i = 0;
-	for (; i < index; i++)
-	{
-		if (n <= 1)
-			return (-1);
-		n = n >> 1;
-	}
-	return (n & 1);
+	if (index < 0 || index > 31)
+		return (-1);
+	return ((n >> index) & 1);
 }
