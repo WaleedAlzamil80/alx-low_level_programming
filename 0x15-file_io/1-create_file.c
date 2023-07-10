57adcb9be259649e42e1907f2352d2736addcdaf
Waleed Alzamil <95117901+WaleedAlzamil80@users.noreply.github.com>
2023-07-11 02:24:12 +0300

    Read text

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strlen - find the length of a string
+ * @s: the string
+ *
+ * Return: integre length of string
+ */
+
+int _strlen(char *s)
+{
+	int  i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: the name of the file
+ * @text_content: the content to be written
+ *
+ * Return: (1) on success and (-1) otherwise
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fOpen;
+	ssize_t num_bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	fOpen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fOpen == -1)
+		return (-1);
+	if (len)
+		num_bytes = write(fOpen, text_content, len);
+	close(fOpen);
+	return (num_bytes == len ? 1 : -1);
+}
